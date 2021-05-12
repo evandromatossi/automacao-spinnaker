8cb28c2b44f881d8d516846fa491737ea1d3b49b
EvandroMatos <42209645+evandromatossi@users.noreply.github.com>
2021-05-12 10:58:31 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    spinnaker = {
+      source = "tidal-engineering/spinnaker"
+      version = "1.0.6"
+    }
+  }
+}
+
+provider "spinnaker" {
+    server = "https://spinnaker-api.viavarejo.com.br"
+}
+
+## Create app in spinnaker
+resource "spinnaker_application" "terraform_example" {
+    application = "automacaoterraform"
+    email       = "evandro.junior@viavarejo.com.br"
+}
+
+## Pipeline deploy-sit
+resource "spinnaker_pipeline" "terraform_example" {
+    application = spinnaker_application.terraform_example.application
+    name        = "Deploy-sit"
+    pipeline    = file("pipelines/create-pipeline-deploy.json")
+}
+
+## Pipeline deploy-hlg
+resource "spinnaker_pipeline" "terraform_example_01" {
+    application = spinnaker_application.terraform_example.application
+    name        = "Deploy-hlg"
+    pipeline    = file("pipelines/create-pipeline-deploy.json")
+}
+
+## Pipeline deploy-prd
+resource "spinnaker_pipeline" "terraform_example_02" {
+    application = spinnaker_application.terraform_example.application
+    name        = "Deploy-prd"
+    pipeline    = file("pipelines/create-pipeline-deploy.json")
+}
+
+## Pipeline rollback-prd
+resource "spinnaker_pipeline" "terraform_example_03" {
+    application = spinnaker_application.terraform_example.application
+    name        = "Rollback-prd"
+    pipeline    = file("pipelines/create-pipeline-deploy.json")
+}
