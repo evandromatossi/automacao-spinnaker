9217256ed175e2ebb3a7a1411c83f54ff3304ffe
EvandroMatos <42209645+evandromatossi@users.noreply.github.com>
2021-05-13 16:41:12 -0300

    Update main.tf

--- spinnaker/main.tf
+++ spinnaker/main.tf
@@ -12,35 +12,35 @@ provider "spinnaker" {
 }
 
 ## Create app in spinnaker
-resource "spinnaker_application" "spinnaker_app" {
+resource "spinnaker_application" "app" {
     application = "var.name_app"
     email       = "var.name_email"
 }
 
 ## Pipeline deploy-sit
 resource "spinnaker_pipeline" "spinnaker_pipeline_sit" {
-    application = spinnaker_application.spinnaker_app.application
+    application = spinnaker_application.app.application
     name        = "Deploy-sit"
     pipeline    = file("create-pipeline-deploy-sit.json")
 }
 
 ## Pipeline deploy-hlg
 resource "spinnaker_pipeline" "spinnaker_pipeline_hlg" {
-    application = spinnaker_application.spinnaker_app.application
+    application = spinnaker_application.app.application
     name        = "Deploy-hlg"
     pipeline    = file("create-pipeline-deploy-hlg.json")
 }
 
 ## Pipeline deploy-prd
 resource "spinnaker_pipeline" "spinnaker_pipeline_prd" {
-    application = spinnaker_application.spinnaker_app.application
+    application = spinnaker_application.app.application
     name        = "Deploy-prd"
     pipeline    = file("create-pipeline-deploy-prd.json")
 }
 
 ## Pipeline rollback-prd
 resource "spinnaker_pipeline" "spinnaker_pipeline_rollback" {
-    application = spinnaker_application.spinnaker_app.application
+    application = spinnaker_application.app.application
     name        = "Rollback-prd"
     pipeline    = file("create-pipeline-deploy-rollback.json")
 }
